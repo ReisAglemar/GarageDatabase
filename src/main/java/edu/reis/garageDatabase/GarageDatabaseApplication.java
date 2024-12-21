3e952aeefe2c0064dde53d8decb78581d470db13
Reis <aglemar.reis@gmail.com>
2024-12-21 12:34:11 -0300

    feat: fiz as configuracoes para rodar no terminal

--- src/main/java/edu/reis/garageDatabase/GarageDatabaseApplication.java
+++ src/main/java/edu/reis/garageDatabase/GarageDatabaseApplication.java
@@ -1,13 +1,19 @@
 package edu.reis.garageDatabase;
 
+import edu.reis.garageDatabase.config.GarageDatabaseApp;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GarageDatabaseApplication {
+public class GarageDatabaseApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(GarageDatabaseApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(GarageDatabaseApplication.class, args);
-	}
-
+    @Override
+    public void run(String... args) throws Exception {
+        GarageDatabaseApp app = new GarageDatabaseApp();
+        app.init();
+    }
 }
