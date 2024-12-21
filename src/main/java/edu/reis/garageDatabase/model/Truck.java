d61b9713fcde2c480d9ce8f477de8d1b8d7a7ab3
Reis <aglemar.reis@gmail.com>
2024-12-21 20:06:32 -0300

    feat: criei a classe para caminhoes

---
+++ src/main/java/edu/reis/garageDatabase/model/Truck.java
@@ -0,0 +1,35 @@
+package edu.reis.garageDatabase.model;
+
+import edu.reis.garageDatabase.erro.register.ExceptionRegister;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+public class Truck extends Vehicle {
+
+    private int loadCapacity;
+
+    public Truck(){}
+
+    public Truck(String brand, String name, String model, String color, int year, float price, String numberPistons,
+                 int loadCapacity) throws ExceptionRegister {
+        super(brand, name, model, color, year, price, numberPistons);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public void details() {
+        String string = super.toString()+
+                """
+                            Capacidade de carga : %d kg
+                            Numero de pistões: %s
+                            Price: R$ %.2f
+                            --------------------------------
+                            ID: %d
+                            Data de inclusão: %s
+                        
+                        """.formatted(loadCapacity, super.getNumberPistonpistons(), super.getPrice(), super.getId(), super.getDateRegistration());
+        System.out.println(string);
+    }
+}
