45acb3499d144773cfecb57a1aa1349e13d0a997
Reis <aglemar.reis@gmail.com>
2024-12-22 11:50:47 -0300

    fix: ajustei quebra de linhas

--- src/main/java/edu/reis/garageDatabase/config/GarageDatabaseApp.java
+++ src/main/java/edu/reis/garageDatabase/config/GarageDatabaseApp.java
@@ -11,14 +11,16 @@ public class GarageDatabaseApp {
     public void init() throws ExceptionRegister {
         System.out.println("Initializing Garage Database");
 
-        Car car = new Car("Honda", "Civic", "LX", "Azul", 2002, 12500.54f, "4", 5);
+        Car car = new Car("Honda", "Civic", "LX", "Azul", 2002,
+                12500.54f, "4", 5);
         car.details();
 
-        Truck truck =new Truck("Scania", "R500", "ATV2", "Cinza", 2022, 250000.00f, "8", 23000);
+        Truck truck =new Truck("Scania", "R500", "ATV2", "Cinza",
+                2022, 250000.00f, "8", 23000);
         truck.details();
 
-        Motorcycle motorcycle = new Motorcycle("Suzuki", "Srad", "sr1000", "Verde", 2023, 50000.00f, "4", 1000);
+        Motorcycle motorcycle = new Motorcycle("Suzuki", "Srad", "sr1000", "Verde",
+                2023, 50000.00f, "4", 1000);
         motorcycle.details();
-
     }
 }
